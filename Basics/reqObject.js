dc5d6316c857fe308db653a4dce127d67d9c7122
arvindjangre <arvindbjangre@gmail.com>
2023-08-27 01:58:14 +0530

    request object, params, query

---
+++ Basics/reqObject.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  console.log(req.query);
+  // console.log(req.params.name);
+  res.status(200).json({
+    message: 'All about request object'
+  });
+});
+
+app.post('/', (req, res) => {
+  console.log(req.body);
+});
+
+app.listen(3000, () => {
+  console.log('Server is started.');
+});
