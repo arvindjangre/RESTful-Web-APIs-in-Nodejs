ec614f92647dfd69ec17a60fd4459ebe09708a36
arvindjangre <arvindbjangre@gmail.com>
2023-08-27 01:41:34 +0530

    headers

--- Basics/dataResponse.js
+++ Basics/dataResponse.js
@@ -16,6 +16,10 @@ const data = [
 ];
 
 const server = http.createServer((req, res) => {
+  res.setHeader('Content-Type', 'application/json');
+  res.setHeader('Content-Language', 'en-us');
+  res.setHeader('Date', new Date(Date.now()));
+  res.setHeader('X-Powered-By', 'Node.js');
   res.end(
     JSON.stringify({
       success: true,
