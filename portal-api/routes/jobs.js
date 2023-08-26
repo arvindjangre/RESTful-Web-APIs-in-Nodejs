d6e703b05a5948bcf813903a650a85dd50050ac5
arvindjangre <arvindbjangre@gmail.com>
2023-08-27 02:35:56 +0530

    creating controllers

--- portal-api/routes/jobs.js
+++ portal-api/routes/jobs.js
@@ -1,12 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/jobs', (req, res) => {
-  res.status(200).json({
-    success : true,
-    message : 'This route will display all jobs in future'
-  });
-});
+// Importing jobs controller methods
+const {getJobs} = require('../controllers/jobsController');
+router.route('/jobs').get(getJobs);
 
 
 module.exports = router;
